e1798ee3243cb7370e99009a14f77b6db2f060a9
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-07-20 09:09:56 +0200

    feat(html_markdown): add new editor

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -4,6 +4,7 @@ use url::Url;
 use crate::serde_helper::string_or_struct;
 
 pub mod css_selector;
+pub mod html_markdown;
 pub mod html_prettify;
 pub mod html_text;
 pub mod regex_replacer;
@@ -14,6 +15,7 @@ pub mod rss;
 pub enum Editor {
     #[serde(deserialize_with = "string_or_struct")]
     CssSelector(css_selector::CssSelector),
+    HtmlMarkdown,
     HtmlPrettify,
     HtmlText,
     RegexReplacer(regex_replacer::RegexReplacer),
@@ -26,7 +28,7 @@ impl Editor {
             Editor::CssSelector(e) => e.is_valid()?,
             Editor::RegexReplacer(e) => e.is_valid()?,
             Editor::Rss(e) => e.is_valid()?,
-            Editor::HtmlPrettify | Editor::HtmlText => {}
+            Editor::HtmlMarkdown | Editor::HtmlPrettify | Editor::HtmlText => {}
         }
         Ok(())
     }
@@ -34,6 +36,7 @@ impl Editor {
     pub fn apply(&self, url: &Url, input: &str) -> anyhow::Result<String> {
         match &self {
             Editor::CssSelector(e) => e.apply(input),
+            Editor::HtmlMarkdown => html_markdown::markdownify(input),
             Editor::HtmlPrettify => html_prettify::prettify(input),
             Editor::HtmlText => html_text::extract(input),
             Editor::RegexReplacer(e) => Ok(e.replace_all(input)?.to_string()),
