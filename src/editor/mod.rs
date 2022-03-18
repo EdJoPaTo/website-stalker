e9dc80e1b3b14a296c7b7c7cc1a4e865b88a3e09
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-03-18 23:51:23 +0100

    refactor: use Self more often

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -33,54 +33,54 @@ pub enum Editor {
 impl Editor {
     pub fn is_valid(&self) -> anyhow::Result<()> {
         match &self {
-            Editor::CssRemove(e) => e.is_valid()?,
-            Editor::CssSelect(e) => e.is_valid()?,
-            Editor::RegexReplace(e) => e.is_valid()?,
-            Editor::Rss(e) => e.is_valid()?,
-            Editor::HtmlMarkdownify
-            | Editor::HtmlPrettify
-            | Editor::HtmlTextify
-            | Editor::HtmlUrlCanonicalize
-            | Editor::JsonPrettify => {}
+            Self::CssRemove(e) => e.is_valid()?,
+            Self::CssSelect(e) => e.is_valid()?,
+            Self::RegexReplace(e) => e.is_valid()?,
+            Self::Rss(e) => e.is_valid()?,
+            Self::HtmlMarkdownify
+            | Self::HtmlPrettify
+            | Self::HtmlTextify
+            | Self::HtmlUrlCanonicalize
+            | Self::JsonPrettify => {}
         }
         Ok(())
     }
 
     pub fn apply(&self, url: &Url, input: &Content) -> anyhow::Result<Content> {
         match &self {
-            Editor::CssRemove(e) => Ok(Content {
+            Self::CssRemove(e) => Ok(Content {
                 extension: Some("html"),
                 text: e.apply(&input.text)?,
             }),
-            Editor::CssSelect(e) => Ok(Content {
+            Self::CssSelect(e) => Ok(Content {
                 extension: Some("html"),
                 text: e.apply(&input.text)?,
             }),
-            Editor::HtmlMarkdownify => Ok(Content {
+            Self::HtmlMarkdownify => Ok(Content {
                 extension: Some("md"),
                 text: html_markdown::markdownify(&input.text),
             }),
-            Editor::HtmlPrettify => Ok(Content {
+            Self::HtmlPrettify => Ok(Content {
                 extension: Some("html"),
                 text: html_pretty::prettify(&input.text)?,
             }),
-            Editor::HtmlTextify => Ok(Content {
+            Self::HtmlTextify => Ok(Content {
                 extension: Some("txt"),
                 text: html_text::textify(&input.text)?,
             }),
-            Editor::HtmlUrlCanonicalize => Ok(Content {
+            Self::HtmlUrlCanonicalize => Ok(Content {
                 extension: Some("html"),
                 text: html_url::canonicalize(url, &input.text)?,
             }),
-            Editor::JsonPrettify => Ok(Content {
+            Self::JsonPrettify => Ok(Content {
                 extension: Some("json"),
                 text: json_prettify::prettify(&input.text)?,
             }),
-            Editor::RegexReplace(e) => Ok(Content {
+            Self::RegexReplace(e) => Ok(Content {
                 extension: input.extension,
                 text: e.replace_all(&input.text)?.to_string(),
             }),
-            Editor::Rss(e) => Ok(Content {
+            Self::Rss(e) => Ok(Content {
                 extension: Some("xml"),
                 text: e.generate(url, &input.text)?,
             }),
