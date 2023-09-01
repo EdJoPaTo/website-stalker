2bbd85f5dfb49626807920cb0c1ed5ca0e2ab657
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-09-01 18:32:54 +0200

    refactor(site_store): hardcode folder
    its basically the same as before just with less runtime state

--- src/site_store.rs
+++ src/site_store.rs
@@ -1,81 +1,69 @@
 use std::ffi::OsString;
 use std::fs::{create_dir_all, read_dir, read_to_string, remove_file, write};
+use std::path::Path;
 
 use crate::ChangeKind;
 
-#[derive(Clone)]
-pub struct SiteStore {
-    folder: String,
+const SITE_FOLDER: &str = "sites";
+
+pub fn remove_gone(expected_basenames: &[String]) -> anyhow::Result<Vec<OsString>> {
+    create_dir_all(SITE_FOLDER)?;
+    let mut superfluous = Vec::new();
+    for file in read_dir(SITE_FOLDER)? {
+        let file = file?;
+        let is_wanted = file
+            .file_name()
+            .into_string()
+            .map_or(false, |name| basename_is_wanted(expected_basenames, &name));
+        if !is_wanted {
+            remove_file(file.path())?;
+            superfluous.push(file.file_name());
+        }
+    }
+    superfluous.sort();
+    Ok(superfluous)
 }
 
-impl SiteStore {
-    pub fn new(folder: String) -> std::io::Result<Self> {
-        create_dir_all(&folder)?;
-        Ok(Self { folder })
+fn remove_same_base_different_extension(basename: &str, extension: &str) -> anyhow::Result<bool> {
+    let mut removed_something = false;
+    for file in read_dir(SITE_FOLDER)? {
+        let file = file?;
+        let remove = file.file_name().into_string().map_or(false, |name| {
+            !name.ends_with(extension) && name.starts_with(&format!("{basename}."))
+        });
+        if remove {
+            remove_file(file.path())?;
+            removed_something = true;
+        }
+    }
+    Ok(removed_something)
+}
+
+pub fn write_only_changed(
+    basename: &str,
+    extension: &str,
+    content: &str,
+) -> anyhow::Result<ChangeKind> {
+    create_dir_all(SITE_FOLDER)?;
+    let path = format!("{}/{basename}.{extension}", self.folder);
+    let content = content.trim().to_string() + "\n";
+
+    let current = read_to_string(&path).unwrap_or_default();
+    let changed = current != content;
+    if changed {
+        write(&path, content)?;
     }
 
-    pub fn remove_gone(&self, expected_basenames: &[String]) -> anyhow::Result<Vec<OsString>> {
-        let mut superfluous = Vec::new();
-        for file in read_dir(&self.folder)? {
-            let file = file?;
-            let is_wanted = file
-                .file_name()
-                .into_string()
-                .map_or(false, |name| basename_is_wanted(expected_basenames, &name));
-            if !is_wanted {
-                remove_file(file.path())?;
-                superfluous.push(file.file_name());
-            }
-        }
-        superfluous.sort();
-        Ok(superfluous)
-    }
+    let removed_something = remove_same_base_different_extension(basename, extension)?;
 
-    pub fn remove_same_base_different_extension(
-        &self,
-        basename: &str,
-        extension: &str,
-    ) -> anyhow::Result<bool> {
-        let mut removed_something = false;
-        for file in read_dir(&self.folder)? {
-            let file = file?;
-            let remove = file.file_name().into_string().map_or(false, |name| {
-                !name.ends_with(extension) && name.starts_with(&format!("{basename}."))
-            });
-            if remove {
-                remove_file(file.path())?;
-                removed_something = true;
-            }
-        }
-        Ok(removed_something)
-    }
-
-    pub fn write_only_changed(
-        &self,
-        basename: &str,
-        extension: &str,
-        content: &str,
-    ) -> anyhow::Result<ChangeKind> {
-        let path = format!("{}/{basename}.{extension}", self.folder);
-        let content = content.trim().to_string() + "\n";
-
-        let current = read_to_string(&path).unwrap_or_default();
-        let changed = current != content;
-        if changed {
-            write(&path, content)?;
-        }
-
-        let removed_something = self.remove_same_base_different_extension(basename, extension)?;
-
-        if removed_something {
-            Ok(ChangeKind::Changed)
-        } else if current.is_empty() {
-            Ok(ChangeKind::Init)
-        } else if changed {
-            Ok(ChangeKind::Changed)
-        } else {
-            Ok(ChangeKind::ContentSame)
-        }
+    if removed_something {
+        Ok(ChangeKind::Changed)
+    } else if current.is_empty() {
+        Ok(ChangeKind::Init)
+    } else if changed {
+        Ok(ChangeKind::Changed)
+    } else {
+        Ok(ChangeKind::ContentSame)
     }
 }
 
