3c1ca490742012e576c3111e6f02c0ce3e99abb9
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-08-18 14:49:52 +0200

    refactor: update to scraper v0.24 & html5ever 0.35

--- src/editor/css_tag_replace.rs
+++ src/editor/css_tag_replace.rs
@@ -1,4 +1,4 @@
-use html5ever::{namespace_url, ns, LocalName, QualName};
+use html5ever::{ns, LocalName, QualName};
 use schemars::JsonSchema;
 use scraper::{Html, Node, Selector};
 use serde::Deserialize;
